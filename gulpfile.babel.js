cde1a21662b0e1df533ec4a6ca4abc3e8195ec1c
Tom Kim <babooteng2@gmail.com>
2021-07-21 14:03:24 +0900

    #1.1 Pug Task part Two

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,21 @@
 import gulp from "gulp";
 import gpug from "gulp-pug"; //https://www.npmjs.com/package/gulp-pug
+import del from "del";
 
 const routes = {
   pug: {
-    src: "src/**/*.pug",
+    src: "src/*.pug",
     dest: "build",
   },
 };
 
-export const pug = () =>
+const pug = () =>
   gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-export const dev = gulp.series([pug]);
+const clean = () => del([routes.pug.dest]);
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([pug]);
+
+export const dev = gulp.series([prepare, assets]); // export는 pakage.json에서 호출되는 것만 쓰면 됨
